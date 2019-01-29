670e1bca348ffb004d780be46b324e4aba2adace
s-ankur <s.ankursonawane@gmail.com>
2019-01-29 19:40:57 +0530

    Fresh Start

--- prac2.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-img=cv2.imread('gal.jpg')
-plt.imshow(img,cmap='gray',interpolation='bicubic')
-plt.xticks([]),plt.yticks([])
-plt.show()
--- pratice.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-img=cv2.imread('gal.jpg')
-plt.imshow(img)
-plt.xticks([]),plt.yticks([])
-plt.show()
